770a50405b75362f7967e4031d4facbac37e90e2
Pawan kumar <kpawan09082005@gmail.com>
2026-05-27 11:45:37 +0530

    fix cors

--- server/server.js
+++ server/server.js
@@ -6,25 +6,32 @@ import authRouter from "./routes/authRoutes.js";
 import rankRouter from "./routes/rankRoutes.js";
 import analysisRouter from "./routes/analysisRoutes.js";
 import { startRankTrackingCron } from "./cron/rankTrackingCron.js";
-
+ 
 connectDB();
-
+ 
 const app = express();
-
-app.use(cors());
+ 
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://ai-saas-website-git-main-pawanproject21.vercel.app",
+    "https://ai-saas-website-pawanproject21.vercel.app",
+  ],
+  credentials: true,
+}));
+ 
 app.use(express.json());
-
+ 
 app.get("/", (req, res) => res.send("Server is running"));
 app.use("/api/auth", authRouter);
 app.use("/api/rank", rankRouter);
 app.use("/api/analysis", analysisRouter);
-
-// STARET CRON JOBS
-
-startRankTrackingCron()
-
+ 
+// START CRON JOBS
+startRankTrackingCron();
+ 
 const PORT = process.env.PORT || 5000;
-
+ 
 app.listen(PORT, () =>
     console.log(`Server running on port ${PORT}`)
 );
